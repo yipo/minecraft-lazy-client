9372db39e785e9f1012ea46df3bbbf15e2f06343
YiPo <YiPo@YiPo-PC>
2012-08-13 02:23:59 +0800

    Add the function of looking for installed `make' tools to Make.bat.
    These tools are supported so far:
    
    - MinGW Make (http://www.mingw.org/)
    - GnuWin32 Make (http://gnuwin32.sourceforge.net/packages/make.htm)

--- Make.bat
+++ Make.bat
@@ -1,6 +1,26 @@
 @ECHO OFF
-PATH %PATH%;C:\MinGW\bin;%~dp0\tool
+
+:: This batch file looks for installed `make' tools
+:: and run it as call the `make' command directly
+:: without setting the PATH environment variable permanently.
+
+PATH %PATH%;%~dp0\tool
 CD /D %~dp0
-mingw32-make %~n1
+
+SET LIST=^
+C:\MinGW\bin\mingw32-make.exe ^
+C:\"Program Files"\GnuWin32\bin\make.exe ^
+C:\"Program Files (x86)"\GnuWin32\bin\make.exe
+
+SET MAKE=make.exe
+FOR %%i IN (%LIST%) DO (
+	IF EXIST %%i SET MAKE=%%i&& GOTO BREAK
+)
+:BREAK
+
+:: If no `make' tool is found, the default command is `make.exe'
+:: Note that specifying `make' only will call this Make.bat recursively.
+
+%MAKE% %*
 IF NOT ERRORLEVEL 1 ECHO [Done]
 PAUSE
